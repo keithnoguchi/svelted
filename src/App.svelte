f4237d459457e8630c48cf4fe76e50a1f9f54751
Keith Noguchi <keith@noguchi.us>
2024-08-04 09:11:45 -0700

    src/Slider: New Slider component
    The component in Svelte is sooo
    easy to understand.  It's just the
    function, in other programming
    languages, e.g. Rust, C, and
    the properties are just the function
    arguments.
    
    I love the current state of the
    JavaScript web development, thanks
    to Svelte.

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Slider from './Slider.svelte';
+
   let product = {
     id: 'svelte-book',
     name: 'Svelte Book',
@@ -31,25 +33,6 @@
   function addToCart(productId) {
     cart = [...cart, productId];
   }
-
-  // For the image slider.
-  let sliderCenterIndex = 0;
-  let sliderLeftIndex = product.images.length - 1;
-  let sliderRightIndex = 1;
-
-  function sliderMoveLeft() {
-    const length = product.images.length;
-    sliderCenterIndex = (sliderCenterIndex - 1 + length) % length;
-    sliderLeftIndex = (sliderCenterIndex - 1 + length) % length;
-    sliderRightIndex = (sliderCenterIndex + 1) % length;
-  }
-
-  function sliderMoveRight() {
-    const length = product.images.length;
-    sliderCenterIndex = (sliderCenterIndex + 1) % length;
-    sliderLeftIndex = (sliderCenterIndex - 1 + length) % length;
-    sliderRightIndex = (sliderCenterIndex + 1) % length;
-  }
 </script>
 
 <header class="header">
@@ -67,29 +50,7 @@
 <article class="product">
   <div class="product-main">
     <div class="image-container">
-      <div class="slider">
-        <img
-          src={product.images[sliderLeftIndex]}
-          alt="The left slider image"
-          class="slider-item left"
-        />
-        <img
-          src={product.images[sliderCenterIndex]}
-          alt="The center slider image"
-          class="slider-item"
-        />
-        <img
-          src={product.images[sliderRightIndex]}
-          alt="The right slider image"
-          class="slider-item right"
-        />
-        <button class="slider-left-button" on:click={sliderMoveLeft}>
-          &lt
-        </button>
-        <button class="slider-right-button" on:click={sliderMoveRight}>
-          &gt
-        </button>
-      </div>
+      <Slider images={product.images} />
     </div>
     <div>
       <h2>{product.name}</h2>
@@ -172,39 +133,5 @@
 
 .image-container img {
   width: 100%;
-}
-
-.slider {
-  position: relative;
-  width: 80%;
-  margin: 0 10%;
-}
-
-.slider-item {
-  width: 100%;
-}
-
-.slider-item.left {
-  position: absolute;
-  top: 0;
-  right: 100%;
-}
-
-.slider-item.right {
-  position: absolute;
-  top: 0;
-  left: 100%;
-}
-
-.slider-left-button {
-  position: absolute;
-  top: 50%;
-  right: 100%;
-}
-
-.slider-right-button {
-  position: absolute;
-  top: 50%;
-  left: 100%;
 }
 </style>
---
+++ src/Slider.svelte
@@ -0,0 +1,72 @@
+<script>
+  export let images;
+
+  let centerIndex = 0;
+  let leftIndex = images.length - 1;
+  let rightIndex = 1;
+
+  function moveLeft() {
+    const length = images.length;
+    centerIndex = (centerIndex -1 + length) % length;
+    leftIndex = (centerIndex - 1 + length) % length;
+    rightIndex = (centerIndex + 1) % length;
+  }
+
+  function moveRight() {
+    const length = images.length;
+    centerIndex = (centerIndex + 1) % length;
+    leftIndex = (centerIndex - 1 + length) % length;
+    rightIndex = (centerIndex + 1) % length;
+  }
+</script>
+
+<div class="slider">
+  <img src={images[leftIndex]} alt="slider image (left)"
+    class="slider-item left" />
+  <img src={images[centerIndex]} alt="slider image (center)"
+    class="slider-item center" />
+  <img src={images[rightIndex]} alt="slider image (right)"
+    class="slider-item right" />
+  <button class="slider-left-button" on:click={moveLeft}>
+    &lt
+  </button>
+  <button class="slider-right-button" on:click={moveRight}>
+    &gt
+  </button>
+</div>
+
+<style>
+  .slider {
+    position: relative;
+    width: 80%;
+    margin: 0 10%;
+  }
+
+  .slider-item {
+    width: 100%;
+  }
+
+  .slider-item.left {
+    position: absolute;
+    top: 0;
+    right: 100%;
+  }
+
+  .slider-item.right {
+    position: absolute;
+    top: 0;
+    left: 100%;
+  }
+
+  .slider-left-button {
+    position: absolute;
+    top: 50%;
+    right: 100%;
+  }
+
+  .slider-right-button {
+    position: absolute;
+    top: 50%;
+    left: 100%;
+  }
+</style>
