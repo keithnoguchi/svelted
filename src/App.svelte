90862bb818f1aa72b9ae259ea635139dd1d2d09a
Keith Noguchi <keith@noguchi.us>
2024-08-08 08:01:00 -0700

    App: Let's play with the spread attributes

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,17 @@
 <script>
   let name = 'Svelted';
-  let src =
-    'https://github.com/sveltejs/branding/raw/master/svelte-logo.png';
-  let alt = "Svelte's logo";
+  let attrs = {
+    src: 'https://github.com/sveltejs/branding/raw/master/svelte-logo.png',
+    alt: "Svelte's logo",
+  };
   let disabled = true;
 </script>
 
-<p class="text">Hello, {name}!</p>
-<img {src} {alt} class="logo" >
-<div>It's {new Date()} now.</div>
+<p class="text">
+  Hello, {name}!
+  It's {new Date()} now.
+</p>
+<img {...attrs} class="logo" >
 <button {disabled}>Click it!</button>
 
 <style>
