33c9602bf9d80052bf0acd41021ab8da6da513a0
Keith Noguchi <keith@noguchi.us>
2024-08-08 07:58:31 -0700

    App: disabled attributes and the bool value

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,13 @@
   let src =
     'https://github.com/sveltejs/branding/raw/master/svelte-logo.png';
   let alt = "Svelte's logo";
+  let disabled = true;
 </script>
 
 <p class="text">Hello, {name}!</p>
 <img {src} {alt} class="logo" >
-
 <div>It's {new Date()} now.</div>
+<button {disabled}>Click it!</button>
 
 <style>
   .logo {height: 6em;}
