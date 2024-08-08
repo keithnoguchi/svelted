2e2b30a82b2f6fa24700287b91e70ae5e4af80a2
Keith Noguchi <keith@noguchi.us>
2024-08-08 07:45:51 -0700

    App: Cleanup the style block
    Let's make the style block only contains
    the logo class style.

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <p class="text">Hello, {name}!</p>
+
 <img
   src="https://github.com/sveltejs/branding/raw/master/svelte-logo.png"
   alt="Svelte's Logo"
@@ -12,19 +13,5 @@
 <div>It's {new Date()} now.</div>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+  .logo {height: 6em;}
 </style>
