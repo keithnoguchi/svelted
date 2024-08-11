02553141e1002a85b7d2d4d861d189c7114d7d84
Keith Noguchi <keith@noguchi.us>
2024-08-11 08:25:32 -0700

    Box: Let's practice the slot in Svelte
    You can pass templates to components.
    
    This is a super powerful feature, similer
    to closures in programming languages.

---
+++ src/Box.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<div class="box">
+  <slot>
+    <em>There is no contents.</em>
+  </slot>
+</div>
+
+<style>
+  .box {
+    border: 1px solid lightgray;
+    padding: 1em;
+  }
+</style>
