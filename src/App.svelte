95496e0aecd167ee18b6f2ff327beea2c6692c69
Keith Noguchi <keith@noguchi.us>
2024-08-29 22:18:50 -0700

    App: $: prefix variables

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,19 @@
 <script>
-  let counter = 0;
+  let x = 1;
 
-  function countUp() {
-    counter += 1;
-  }
+  $: area = x * x;
+  $: volume = x * x * x;
 </script>
 
-<button on:click={countUp}>
-  {counter} clicked!
-</button>
+<div>
+  Length:
+  <button on:click={() => (x -= 1)} disabled={x <= 1}>
+    -
+  </button>
+  {x}m
+  <button on:click={() => (x += 1)}>
+    +
+  </button>
+</div>
+<div>Area: {area}m<sup>2</sup></div>
+<div>Volume: {volume}m<sup>3</sup></div>
