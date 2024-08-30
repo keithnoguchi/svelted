6fd3b174c402717f1ad0f9d5c057643b9f06fab6
Keith Noguchi <keith@noguchi.us>
2024-08-29 21:49:54 -0700

    App: let's start playing with the reactivity
    State + DOM = Reactivity.

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,11 @@
 <script>
-  import { tick } from 'svelte';
+  let counter = 0;
 
-  let clicked = false;
-
-  async function handler() {
-    clicked = true;
-
-    await tick();
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    ctx.fillStyle = '#ddd';
-    ctx.fillRect(0, 0, 100, 200);
-    ctx.fillRect(100, 100, 200, 200);
+  function countUp() {
+    counter += 1;
   }
 </script>
 
-<div>
-  <button on:click={handler}>Display</button>
-</div>
-
-{#if clicked}
-  <canvas id="canvas" width="200" height="200" />
-{/if}
-
-<style>
-</style>
+<button on:click={countUp}>
+  {counter} clicked!
+</button>
