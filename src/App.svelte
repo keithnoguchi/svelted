d7426c7584575393b58a561d0a61c6dfa60ba4a0
Keith Noguchi <keith@noguchi.us>
2024-09-15 08:24:33 -0700

    Let's explore a bit of HTML5

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,14 @@
-<script>
-  let n = 1;
-
-  $: if (n > 10) {
-    alert('Too big number.  Set it less than or equal to 10.');
-    n = 10;
-  }
-</script>
-
-<div>
-  Number: {n}
-</div>
-<div>
-  <button on:click={() => (n -= 1)} disabled={n <= 1}>
-    -
-  </button>
-  <button on:click={() => (n += 1)}>
-    +
-  </button>
-</div>
+<form>
+  <fieldset>
+    <legend>Some bits of legend</legend>
+    <div>
+      <label for=email>Email:</label>
+      <input id=email type=email />
+     </div>
+     <div>
+      <label for=url>URL:</label>
+      <input id=url type=url />
+     </div>
+     <input type=submit value="Save session" formnovalidation />
+  </fieldset>
+</form>
